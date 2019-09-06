e5438cf9d61cc5ffa9d6c03df8a31ad118ee1167
ejaz <ejaz>
2019-09-06 11:23:09 +0530

    Removed Client OverHead

--- NPLab/DHCP/Client.java
+++ NPLab/DHCP/Client.java
@@ -3,53 +3,24 @@ import java.net.*;
 
 class Client{
 
+String SendMessage( String message ) throws Exception {
 
-String Request() throws Exception {
-  String broadcastMessage="Request";
-  InetAddress address=InetAddress.getByName("255.255.255.255");
+       InetAddress address=InetAddress.getByName("255.255.255.255");
        DatagramSocket  socket = new DatagramSocket();
        socket.setBroadcast(true);
-
-       byte[] buffer = broadcastMessage.getBytes();
-
+       byte[] buffer = message.getBytes();
        DatagramPacket packet
          = new DatagramPacket(buffer, buffer.length, address, 4445);
        socket.send(packet);
        socket.close();
          byte[] remoteInput = new byte[65535];
-      DatagramPacket DpReceive = new DatagramPacket(remoteInput, remoteInput.length);
-      DatagramSocket serverSocket = new DatagramSocket(2121);
+       DatagramPacket DpReceive = new DatagramPacket(remoteInput, remoteInput.length);
+       DatagramSocket serverSocket = new DatagramSocket(2121);
        serverSocket.receive(DpReceive);
        serverSocket.close();
        socket.close();
       return data(remoteInput).toString();
-
-  }
-
-
-
-  String Discovery() throws Exception {
-    String broadcastMessage="Discover";
-         InetAddress address=InetAddress.getByName("255.255.255.255");
-         DatagramSocket  socket = new DatagramSocket();
-         socket.setBroadcast(true);
-
-         byte[] buffer = broadcastMessage.getBytes();
-
-         DatagramPacket packet
-           = new DatagramPacket(buffer, buffer.length, address, 4445);
-         socket.send(packet);
-         socket.close();
-        byte[] remoteInput = new byte[65535];
-        DatagramPacket DpReceive = new DatagramPacket(remoteInput, remoteInput.length);
-        DatagramSocket serverSocket = new DatagramSocket(2121);
-         serverSocket.receive(DpReceive);
-         socket.close();
-         serverSocket.close();
-         return data(remoteInput).toString();
-
-    }
-
+}
 
     public static StringBuilder data(byte[] a)
        {
@@ -69,10 +40,9 @@ String Request() throws Exception {
  public static void main(String[] args){
 try {
    Client tmp = new Client();
-
-   String disResp = tmp.Discovery();
+   String disResp = tmp.SendMessage("Discover");
    System.out.println(disResp);
-   String reqResponse = tmp.Request();
+   String reqResponse = tmp.SendMessage("Request");
    System.out.println(reqResponse);
  }
  catch(Exception e) {
