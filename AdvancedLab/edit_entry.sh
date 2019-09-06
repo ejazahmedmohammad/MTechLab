c074bd6925e8855f3968a1c1d1fedb20de094689
ejaz <ejaz>
2019-09-06 09:32:26 +0530

    Added input for edit entry

--- AdvancedLab/edit_entry.sh
+++ AdvancedLab/edit_entry.sh
@@ -10,6 +10,9 @@ echo enter line number
 read line_num
 echo enter new name
 read new_name
+echo enter new number
+read new_num
+echo sed -i  "$1"   addressBook|bash
 sed -i s/$old_name/$new_name/g addressBook ;;
   2) 
   
